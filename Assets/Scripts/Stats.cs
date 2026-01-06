2b7a01a559693d4c99d1f3d297466f5100129dbf
wit117 <blasolgd5901@gmail.com>
2026-01-07 08:47:43 +0900

    Add simple stat class

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Stats
+{
+    
+    public float maxHealth = 100f;
+    public float health = 100f;
+    
+    public float baseDamage = 10f;
+    public float defense = 0f;
+    public float moveSpeed = 5f;
+
+}
